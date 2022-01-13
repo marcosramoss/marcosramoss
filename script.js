1db543feab1a1f285ca7ce6781ad31c95221baec
Marcos Ramos <mksvncs@gmail.com>
2022-01-13 20:21:11 -0300

    adicionei mais conteudo e menu ativo

---
+++ script.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll('nav a')
+function ativarLink(link) {
+    const url = location.href
+    const href = link.href
+    if (url === href) {
+        link.classList.add('ativo')
+    }
+}
+links.forEach(ativarLink)
